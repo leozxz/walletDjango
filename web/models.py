652a1cebe47b9b56fc755e0f40f066cda6da7850
Leonardo de Oliveira Andrade <52091203+leozxz@users.noreply.github.com>
2020-12-01 15:16:54 -0300

    Update models.py

--- web/models.py
+++ web/models.py
@@ -1,1 +1,13 @@
+from django.db import models
+
+class Moeda(models.Model):
+    nome_moeda = models.CharField(max_length=15)
+    valor_moeda = models.DecimalField(max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.nome_moeda
+
+class Transacao(models.Model):
+    real = models.DecimalField(max_digits=9, decimal_places=2)
+    cripto = models.DecimalField(max_digits=9, decimal_places=2)
 
